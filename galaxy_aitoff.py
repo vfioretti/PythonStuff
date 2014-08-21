78f236f32ab83471f464f70009797dc0bd28b568
Valentina Fioretti <fioretti@iasfbo.inaf.it>
2014-08-21 15:55:03 +0200

    Fill and colors added

--- galaxy_aitoff.py
+++ galaxy_aitoff.py
@@ -2,7 +2,7 @@
  galaxy_aitoff.py  -  description
  ---------------------------------------------------------------------------------
  Function to plot the galaxy in galactic coordinates and AITOFF projection.
- It is possible to plot a set of squares/rings or points as additional feature.
+ It is possible to plot a set of rings or points as additional feature.
  ---------------------------------------------------------------------------------
  copyright            : (C) 2014 Valentina Fioretti
  email                : fioretti@iasfbo.inaf.it
@@ -17,10 +17,12 @@
  - vip_sources: flag to load the most famous Gamma-ray sources (Crab, Vela, Geminga, 3C 454.3)
  ---------------------------------------------------------------------------------
  Required data input format: ASCII file (one entry each row)
+ Optional parameters: color keyword c_1 (integer) for points and fill keyword (0/1) plus fill color 
+ (integer) for the rings (f_1 and fc_1)
  - Sources:
- l_1 b_1
+ l_1 b_1 (c_1)
  - Rings:
- l_1 b_1 r_1 
+ l_1 b_1 r_1 (f_1 fc_1)
  ---------------------------------------------------------------------------------
  Caveats:
  None
@@ -36,14 +38,19 @@
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
+import matplotlib.cm as cmx
+import matplotlib.colors as cmc
 import numpy as np
 from math import pi
 import sys
 
 
+
 def plgal(title='', source_list='', square_list = '', ring_list='', vip_sources=0):
 	deg2rad = np.pi/180.
-	
+	cmap = cmx.jet  ##I set colomap to 'jet' 
+	norm = cmc.Normalize(vmin=0, vmax=10) 
+
 	# converting latitude to be plotted in the 180, 90, 0, 270, 180 axis
 	def convert_l(l_in):
 			if ((l_in >= 0.) & (l_in <= 180.)):
@@ -62,31 +69,54 @@ def plgal(title='', source_list='', square_list = '', ring_list='', vip_sources=
 	if source_list:
 		l_source = []
 		b_source = []
+		c_source = []
 		f_read = open(source_list, 'r')
 		for line in f_read:
 			line = line.strip()
 			columns = line.split()
+			n_cols = len(columns)
 			columns[0] = float(columns[0])  # converting from string to float
 			columns[1] = float(columns[1])
+			if (n_cols > 2):
+				columns[2] = int(columns[2])
+				c_source.append(columns[2])
+			else:
+				c_source.append(0)
 			l_in = convert_l(columns[0])*deg2rad
 			b_in = columns[1]*deg2rad
 			l_source.append(l_in) 
 			b_source.append(b_in)
-		ax.scatter(l_source, b_source, s=50)
+		ax.scatter(l_source, b_source, s=30, c=c_source)
 
 	# Loading the rings
 	if ring_list:
 		f_read = open(ring_list, 'r')
 		for line in f_read:
+			f_ring = 'False'
+			fc_ring = 'none'
 			line = line.strip()
 			columns = line.split()
+			n_cols = len(columns)
 			columns[0] = float(columns[0])  # converting from string to float
 			columns[1] = float(columns[1])
 			columns[2] = float(columns[2])
+			if (n_cols > 3):
+				columns[3] = int(columns[3])
+				if (columns[3] == 1):
+					f_ring = 'True'
+				else:
+					f_ring = 'False'
+					fc_ring = 'none'
+			if (n_cols > 4):
+				columns[4] = int(columns[4])
+				if (columns[3] == 1): 
+					fc_ring = cmap(norm(columns[4])) 
+				else: 
+					fc_ring = 'none'
 			l_in = convert_l(columns[0])
 			b_in = columns[1]
 			r_in = columns[2]
-			ax.add_artist(Circle(xy=((l_in)*deg2rad, (b_in)*deg2rad), facecolor='none', fill='False', edgecolor='k', radius=r_in*deg2rad)) 
+			ax.add_artist(Circle(xy=((l_in)*deg2rad, (b_in)*deg2rad), facecolor=fc_ring, fill=f_ring, edgecolor='k', radius=r_in*deg2rad)) 
 
 	# Loading the vip sources	
 	if vip_sources:
